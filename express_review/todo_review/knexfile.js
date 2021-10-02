bdba57f208c08a852f3ab927a925e98d0acc9c01
stephanie-warmenhoven <steph_war@yahoo.ca>
2021-10-02 10:46:31 -0700

    Create Express app with express generator

---
+++ express_review/todo_review/knexfile.js
@@ -0,0 +1,17 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      database: 'todo_review'
+      // Linux users must include db username and password
+      // username: '',
+      // password: ''
+    }
+  }
+
+  
+
+};
