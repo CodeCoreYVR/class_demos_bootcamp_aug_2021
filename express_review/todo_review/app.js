68d164006edd7b5a7a8531030e6e45ee4795c74b
stephanie-warmenhoven <steph_war@yahoo.ca>
2021-10-02 10:56:18 -0700

    Initialize home route and view with partials

--- express_review/todo_review/app.js
+++ express_review/todo_review/app.js
@@ -4,8 +4,8 @@ const path = require('path'); //*
 const cookieParser = require('cookie-parser'); //*
 const logger = require('morgan'); //*
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+// const indexRouter = require('./routes/index');
+// const usersRouter = require('./routes/users');
 
 const app = express(); //*
 
@@ -19,8 +19,12 @@ app.use(express.urlencoded({ extended: false })); //*
 app.use(cookieParser()); //*
 app.use(express.static(path.join(__dirname, 'public'))); //*
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -37,6 +41,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
 
 //Add server Domain and Host:
 const PORT = 4545;
