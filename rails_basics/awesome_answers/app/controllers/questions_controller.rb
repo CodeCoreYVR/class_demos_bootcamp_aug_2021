c50599712e6141419f0a1b6412c85710c5875452
stephanie-warmenhoven <steph_war@yahoo.ca>
2021-10-20 12:08:22 -0700

    Add selectize and tag getter and setter methods and views

--- rails_basics/awesome_answers/app/controllers/questions_controller.rb
+++ rails_basics/awesome_answers/app/controllers/questions_controller.rb
@@ -71,7 +71,7 @@ class QuestionsController < ApplicationController
     end
     
     def question_params
-        params.require(:question).permit(:title,:body, tag_ids: [])
+        params.require(:question).permit(:title,:body,:tag_names)
     end
 
     def authorize_user!
