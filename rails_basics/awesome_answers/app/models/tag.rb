f69cb6fb6cda93e74ad608c4a4f926c10b597dfc
stephanie-warmenhoven <steph_war@yahoo.ca>
2021-10-20 12:58:54 -0700

    Added some styling for tags

--- rails_basics/awesome_answers/app/models/tag.rb
+++ rails_basics/awesome_answers/app/models/tag.rb
@@ -6,7 +6,7 @@ class Tag < ApplicationRecord
     has_many :questions, through: :taggings
 
     #VALIDATIONS
-    validates :name, presence: true #, uniqueness: { case_sensitive: false }
+    validates :name, presence: true, uniqueness: { case_sensitive: false }
 
     #the case sensitive option will make uniqueness validation ignore the case i.e. record of "Science" and "SCIENCE" can't co-exist
 
