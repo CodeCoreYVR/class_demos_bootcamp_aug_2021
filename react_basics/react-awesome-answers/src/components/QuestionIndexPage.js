07357a3f5e8768ec68c615fea0996293ecd5446d
Mao Li <anrenyil@gmail.com>
2021-11-09 11:12:31 -0800

    react state delete a question

--- react_basics/react-awesome-answers/src/components/QuestionIndexPage.js
+++ react_basics/react-awesome-answers/src/components/QuestionIndexPage.js
@@ -5,8 +5,28 @@ class QuestionIndexPage extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            questions: questionIndexData
+            questions: questionIndexData,
+            location: "VAN",
+            name: "CodeCore"
         }
+    }
+
+    deleteQuestion(id) {
+        // anytime you want to change the state, you must use this.setState function!
+        // you can not use this.state = sth!
+        // this callback function has 2 arguments
+        // 1. the state, 2. the props
+        // the return of this callback function will be merged into the state
+        // `merge` => merge these 2 objects and only replace the same name attributes
+
+        // this.setState((state, props) => {
+        //     return {
+        //         questions: state.questions.filter(q => q.id !== id)
+        //     }
+        // })
+        this.setState({
+            questions: this.state.questions.filter(q => q.id !== id)
+        })
     }
 
     render() {
@@ -14,7 +34,7 @@ class QuestionIndexPage extends Component {
             <div>
                 {this.state.questions.map((e) => {
                     return (
-                        <h1 key={e.id}>{e.id} - {e.title}</h1>
+                        <h1 key={e.id}>{e.id} - {e.title} <button onClick={() => this.deleteQuestion(e.id)}>Delete</button> </h1>
                     )
                 })}
             </div>
