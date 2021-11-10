48647a93edee816b9e396dc9a2497e0724c98ee4
stephanie-warmenhoven <steph_war@yahoo.ca>
2021-11-10 10:34:27 -0800

    Lifecycle Methods CurrentDateTime component demo

--- react_basics/react-awesome-answers/src/App.js
+++ react_basics/react-awesome-answers/src/App.js
@@ -1,14 +1,37 @@
+import React, {Component} from 'react';
 import './App.css';
-import QuestionShowPage from './components/QuestionShowPage';
-import QuestionIndexPage from './components/QuestionIndexPage';
+// import QuestionShowPage from './components/QuestionShowPage';
+// import QuestionIndexPage from './components/QuestionIndexPage';
+import CurrentDateTime from './components/CurrentDateTime';
 
-function App() {
-  return (
-    <div>
-      <QuestionIndexPage />
-      <QuestionShowPage />
-    </div>
-  );
+// function App() {
+//   return (
+//     <div>
+//       <QuestionIndexPage />
+//       <QuestionShowPage />
+//     </div>
+//   );
+// }
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      clocksCount: [1] //an array of something
+    }
+  }
+  render(){
+    return(
+      <div>
+        {
+          this.state.clocksCount.map((count, index) => {
+            return <CurrentDateTime key={index} shouldShowTime={true}/>
+          })
+        }
+      </div>
+      
+    )
+  }
 }
 
 export default App;
