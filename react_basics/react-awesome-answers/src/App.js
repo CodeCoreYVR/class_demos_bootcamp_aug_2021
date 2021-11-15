1195bb75caa821afcdbeb7d46f50f30f1ba49058
Mao Li <anrenyil@gmail.com>
2021-11-15 11:57:52 -0800

    react hooks refactor app.js

--- react_basics/react-awesome-answers/src/App.js
+++ react_basics/react-awesome-answers/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import QuestionShowPage from './components/QuestionShowPage';
 import QuestionIndexPage from './components/QuestionIndexPage';
@@ -14,77 +14,49 @@ import SignUpPage from './components/SignUpPage';
 import UseStateHook from './components/UseStateHook';
 import UseEffectHook from './components/UseEffectHook';
 
+export default function App() {
+  const [user, setUser] = useState(null);
 
-// function App() {
-//   return (
-//     <div>
-//       <QuestionIndexPage />
-//       <QuestionShowPage />
-//     </div>
-//   );
-// }
+  useEffect(() => {
+    getCurrentUser();
+  }, [])
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      // clocksCount: [1], //an array of something
-      user: null
-    }
-  }
-
-  componentDidMount() {
-    this.getCurrentUser()
-  }
-
-  getCurrentUser = () => {
+  const getCurrentUser = () => {
     return User.current().then(user => {
-      //This is the safe navigation opreator
-      //Similar to user && user.id
       if (user?.id) {
-        this.setState(state => {
-          return { user }
-        })
+        setUser(user)
       }
     })
   }
+  const onSignOut = () => { setUser(null) }
+  return (
 
-  onSignOut = () => { this.setState({ user: null }) }
+    <BrowserRouter>
+      <NavBar currentUser={user} onSignOut={onSignOut} />
+      <Switch>
+        <Route exact path='/sign_in'
+          render={(routeProps) => <SignInPage {...routeProps} onSignIn={getCurrentUser} />}>
+        </Route>
+        <Route
+          exact path='/sign_up'
+          render={(routeProps) => <SignUpPage {...routeProps} onSignUp={getCurrentUser} />}
+        >
+        </Route>
+        <Route exact path='/questions' component={QuestionIndexPage} />
+        <AuthRoute isAuthenticated={!!user} path='/questions/new' component={NewQuestionPage}></AuthRoute>
+        <Route path='/questions/:id' component={QuestionShowPage} ></Route>
+        <Route path='/use_state' component={UseStateHook} />
+        <Route path='/use_effect' component={UseEffectHook} />
+        <Route
+          path="*"
+          element={
+            <main style={{ padding: "1rem" }}>
+              <p>There's nothing here!</p>
+            </main>
+          }
+        />
+      </Switch>
+    </BrowserRouter>
 
-  render() {
-    return (
-
-      <BrowserRouter>
-        <NavBar currentUser={this.state.user} onSignOut={this.onSignOut} />
-        <Switch>
-          <Route exact path='/sign_in'
-            render={(routeProps) => <SignInPage {...routeProps} onSignIn={this.getCurrentUser} />}>
-          </Route>
-          <Route
-            exact path='/sign_up'
-            render={(routeProps) => <SignUpPage {...routeProps} onSignUp={this.getCurrentUser} />}
-          >
-          </Route>
-          <Route exact path='/questions' component={QuestionIndexPage} />
-          <AuthRoute isAuthenticated={!!this.state.user} path='/questions/new' component={NewQuestionPage}></AuthRoute>
-          <Route path='/questions/:id' component={QuestionShowPage} ></Route>
-          <Route path='/use_state' component={UseStateHook} />
-          <Route path='/use_effect' component={UseEffectHook} />
-          <Route
-            path="*"
-            element={
-              <main style={{ padding: "1rem" }}>
-                <p>There's nothing here!</p>
-              </main>
-            }
-          />
-        </Switch>
-      </BrowserRouter>
-
-    )
-  }
+  )
 }
-
-//The "*" has special meaning here. It will match only when no other routes do.
-
-export default App;
