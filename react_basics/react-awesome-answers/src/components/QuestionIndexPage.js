f83d099b7dec296be92652e745cf129a1042a28c
Mao Li <anrenyil@gmail.com>
2021-11-15 11:20:52 -0800

    react hooks refactor questionindexpage

--- react_basics/react-awesome-answers/src/components/QuestionIndexPage.js
+++ react_basics/react-awesome-answers/src/components/QuestionIndexPage.js
@@ -1,86 +1,49 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 // import questionIndexData from '../mock_data/questionIndexData';
 import NewQuestionForm from './NewQuestionForm';
 import { Question } from '../requests';
 import { Link } from 'react-router-dom'
 
-class QuestionIndexPage extends Component {
-    constructor(props) {
-        super(props);
-        // this.state = {
-        //     questions: questionIndexData,
-        //     location: "VAN",
-        //     name: "CodeCore"
-        // }
-        this.state = {
-            questions: []
-        }
-        this.createQuestion = this.createQuestion.bind(this)
-    }
+export default function QuestionIndexPage() {
+    const [questions, setQuestions] = useState([]);
 
-    componentDidMount(){
+    useEffect(() => {
         Question.index()
-        .then((questionsData) => {
-            this.setState((state)=> {
-                return {
-                    questions: questionsData
-                }
+            .then((questionsData) => {
+                setQuestions(questionsData);
             })
-        })
-    }
+    }, [])
 
-    deleteQuestion(id) {
-        // anytime you want to change the state, you must use this.setState function!
-        // you can not use this.state = sth!
-        // this callback function has 2 arguments
-        // 1. the state, 2. the props
-        // the return of this callback function will be merged into the state
-        // `merge` => merge these 2 objects and only replace the same name attributes
-
-        // this.setState((state, props) => {
-        //     return {
-        //         questions: state.questions.filter(q => q.id !== id)
-        //     }
-        // })
-        this.setState({
-            questions: this.state.questions.filter(q => q.id !== id)
-        })
-    }
-    createQuestion(params) {
-
-        this.setState((state, props) => {
-            return {
-                questions: [
-                    ...state.questions, // copy all the existing questions
-                    {
-                        id: (Math.max(...state.questions.map(q => q.id)) + 1),
-                        // title: params.title,
-                        //  body: params.body
-                        ...params
-                    }
-                ]
-            }
-        })
+    const deleteQuestion = (id) => {
+        setQuestions(
+            questions.filter(q => q.id !== id)
+        )
         // this.setState({
-        //     questions:[
-        //         ...this.state.questions,
-
-        //     ]
+        //     questions: this.state.questions.filter(q => q.id !== id)
         // })
     }
-    render() {
-        return (
-            <div>
-                {this.state.questions.map((e) => {
-                    return (
-                        <h1 key={e.id}>{e.id} - <Link to={`/questions/${e.id}`}>{e.title}</Link> <button onClick={() => this.deleteQuestion(e.id)}>Delete</button> </h1>
-                    )
-                })}
-                <NewQuestionForm createQuestion={this.createQuestion} />
-                {/* <NewQuestionForm createQuestion={(params) => this.createQuestion(params)} /> */}
-            </div>
+
+    const createQuestion = (params) => {
+        setQuestions(
+            [
+                ...questions,
+                {
+                    id: (Math.max(...questions.map(q => q.id)) + 1),
+                    ...params
+                }
+            ]
         )
     }
-}
 
-export default QuestionIndexPage;
+    return (
+        <div>
+            {questions.map((e) => {
+                return (
+                    <h1 key={e.id}>{e.id} - <Link to={`/questions/${e.id}`}>{e.title}</Link> <button onClick={() => deleteQuestion(e.id)}>Delete</button> </h1>
+                )
+            })}
+            <NewQuestionForm createQuestion={createQuestion} />
+            {/* <NewQuestionForm createQuestion={(params) => this.createQuestion(params)} /> */}
+        </div>
+    )
+}
