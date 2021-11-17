e1c85728d7ea1bd16e10abdaab80f57814dddc9e
stephanie-warmenhoven <steph_war@yahoo.ca>
2021-11-17 10:06:45 -0800

    Handle Record not Found API

--- rails_basics/awesome_answers/app/controllers/api/application_controller.rb
+++ rails_basics/awesome_answers/app/controllers/api/application_controller.rb
@@ -1,6 +1,8 @@
 class Api::ApplicationController < ApplicationController
     skip_before_action :verify_authenticity_token
 
+    rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
     #Error message handling
     #To send a json error message when user types in, for example: localhost:3000/api/v1/somethingwrong
     def not_found
@@ -22,6 +24,25 @@ class Api::ApplicationController < ApplicationController
         unless current_user.present?
             render(json:{status: 401})
         end
+    end
+
+    protected
+    #protected is like a private except thst it prevents
+    #decendent classes from using the protected methods
+
+    def record_not_found(error)
+        render(
+            status: 404, #alias :not_found
+            json: {
+                status: 404,
+                errors: [
+                    {
+                        type: error.class.to_s,
+                        message: error.message
+                    }
+                ]
+            }
+        )
     end
     
 end
