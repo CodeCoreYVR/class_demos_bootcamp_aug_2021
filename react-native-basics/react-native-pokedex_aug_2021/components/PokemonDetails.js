abd48389e0f0cc638e07c05707650e48cb4560d0
Mao Li <anrenyil@gmail.com>
2021-11-22 12:45:21 -0800

    fetch pokemon details

--- react-native-basics/react-native-pokedex_aug_2021/components/PokemonDetails.js
+++ react-native-basics/react-native-pokedex_aug_2021/components/PokemonDetails.js
@@ -1,10 +1,44 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
 
 export default function PokemonDetails(props) {
+    const [pokemon, setPokemon] = useState({});
+    const params = props.route.params;
+    useEffect(() => {
+        fetch(params.pokemon.url)
+            .then(res => res.json())
+            .then(data => {
+                setPokemon(data);
+            })
+    }, [])
     return (
-        <View>
-
+        <View style={styles.container}>
+            <Button title="Go Back" onPress={() => {
+                props.navigation.goBack();
+            }} />
+            <Image source={{
+                url: pokemon.id ? pokemon.sprites.front_default : ""
+            }}
+                style={styles.image}
+            />
+            <Text style={styles.h1}>{pokemon.name}</Text>
+            <Text>Height: {pokemon.height}</Text>
+            <Text>Weight: {pokemon.weight}</Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "flex-start",
+        alignItems: "center"
+    },
+    image: {
+        height: 300,
+        width: 300
+    },
+    h1: {
+        fontSize: 30
+    }
+})
