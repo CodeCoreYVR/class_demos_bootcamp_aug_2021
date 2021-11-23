9830e5a9cef7fb930e7f4213a1f4a15653f9470c
Mao Li <anrenyil@gmail.com>
2021-11-23 10:53:19 -0800

    gift user page

---
+++ react_basics/react-awesome-answers/src/components/GiftUserForm.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { useStripe, useElements, CardElement } from '@stripe/react-stripe-js';
+import './css/GiftUserForm.css'
+
+const CARD_ELEMENT_OPTIONS = {
+    style: {
+        base: {
+            color: "#32325d",
+            fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+            fontSmoothing: "antialiased",
+            fontSize: "16px",
+            "::placeholder": {
+                color: "#aab7c4",
+            },
+        },
+        invalid: {
+            color: "#fa755a",
+            iconColor: "#fa755a",
+        },
+    },
+};
+
+export default function GiftUserForm() {
+    const stripe = useStripe();
+    const elements = useElements();
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        if (!stripe || !elements) {
+            return;
+        }
+
+        const card = elements.getElement(CardElement);
+        const result = await stripe.createToken(card);
+
+        if (result.error) {
+            console.log(result.error.message);
+        } else {
+            // Send the token to your server.
+            // get the amount and send the request to Rails API
+
+        }
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label htmlFor="amount">Amount:</label>
+                <input type="number" name="amount" id="amount" height="40px" />
+            </div>
+            <label>
+                Card details
+                <CardElement options={CARD_ELEMENT_OPTIONS} />
+            </label>
+            <button disabled={!stripe}>Confirm order</button>
+        </form>
+    );
+}
